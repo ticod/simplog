959980cd2df1d73514bd7d2bc0508df3e2f2169d
tico <58782736+ticod@users.noreply.github.com>
2020-12-23 18:18:50 +0900

    Delete temp files
    임시 파일들 삭제

--- src/main/java/com/ticodev/TestTutorial.java
+++
@@ -1,9 +0,0 @@
-package com.ticodev;
-
-public class TestTutorial {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-}
--- src/test/java/com/ticodev/TestTutorialTest.java
+++ src/test/java/com/ticodev/TestTutorialTest.java
@@ -1,7 +1,5 @@
 package com.ticodev;
 
-import com.ticodev.TestTutorial.*;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
