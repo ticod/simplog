ac7ec8abbe1a45cf829eca2238105868a425d2ef
tico <58782736+ticod@users.noreply.github.com>
2020-12-23 23:58:07 +0900

    Add DbConnection.java
    MyBatis Db Connection 클래스 추가

---
+++ src/main/java/com/ticodev/DbConnection.java
@@ -0,0 +1,37 @@
+package com.ticodev;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DbConnection {
+
+    private static final String resource = "com/ticodev/model/mybatis-config.xml";
+    private static final SqlSessionFactory sqlMap;
+
+    private DbConnection() {}
+
+    static {
+        InputStream input = null;
+
+        try {
+            input = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sqlMap = new SqlSessionFactoryBuilder().build(input);
+    }
+
+    public static SqlSession getConnection() {
+        return sqlMap.openSession();
+    }
+
+    public static void close(SqlSession session) {
+        session.commit();
+        session.close();
+    }
+}
