8182103f9489cdb5cb4169497c438de8de911229
tico <58782736+ticod@users.noreply.github.com>
2020-12-27 03:25:15 +0900

    Add LogoutAction

---
+++ src/main/java/com/ticodev/action/login/LogoutAction.java
@@ -0,0 +1,25 @@
+package com.ticodev.action.login;
+
+import com.ticodev.action.Action;
+import com.ticodev.action.ActionForward;
+import com.ticodev.model.dao.MemberDao;
+import com.ticodev.model.dto.Member;
+import com.ticodev.util.HashEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutAction implements Action {
+
+    MemberDao dao = new MemberDao();
+
+    @Override
+    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        request.getSession().invalidate();
+        request.setAttribute("msg", "로그아웃하셨습니다");
+        request.setAttribute("url", "index.do");
+
+        return new ActionForward(false, "/alert.jsp");
+    }
+}
