9113905745ffbaa1d2517c8d65e749f647aaebfd
tico <58782736+ticod@users.noreply.github.com>
2020-12-27 03:35:15 +0900

    Add UserLoginCheckAction
    유저 로그인 확인하는 Action 생성

---
+++ src/main/java/com/ticodev/action/login/UserLoginCheckAction.java
@@ -0,0 +1,30 @@
+package com.ticodev.action.login;
+
+import com.ticodev.action.Action;
+import com.ticodev.action.ActionForward;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserLoginCheckAction implements Action {
+
+    protected String sessionId;
+
+    @Override
+    public ActionForward execute(HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+
+        sessionId = (String) request.getSession().getAttribute("login");
+
+        /* 로그아웃 상태 */
+        if (sessionId == null) {
+            request.setAttribute("msg", "로그인 후 이용해주세요");
+            request.setAttribute("url", "login_form.do");
+            return new ActionForward(false, "/alert.jsp");
+        }
+
+        return new ActionForward();
+    }
+
+}
