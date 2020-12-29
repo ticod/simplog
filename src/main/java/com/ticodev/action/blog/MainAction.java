363676ed69b4b3dd8198761dc53350fffd61c07d
tico <58782736+ticod@users.noreply.github.com>
2020-12-29 17:23:19 +0900

    Update 블로그 메인 화면 Action

--- src/main/java/com/ticodev/action/blog/MainAction.java
+++ src/main/java/com/ticodev/action/blog/MainAction.java
@@ -1,16 +1,19 @@
 package com.ticodev.action.blog;
 
-import com.ticodev.action.Action;
 import com.ticodev.action.ActionForward;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MainAction implements Action {
-    @Override
-    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+public class MainAction extends BlogUrlPreprocessor {
 
-        BlogUrlProcessor.processURI(request);
-        return BlogUrlProcessor.getActionForward(request);
+    @Override
+    public ActionForward doExecute(HttpServletRequest request,
+                                   HttpServletResponse response)
+            throws Exception {
+
+        return getActionForward();
+
     }
+
 }
