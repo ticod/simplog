9aab543bf0657eb4cfa0ae90a6161ae889912c8d
tico <58782736+ticod@users.noreply.github.com>
2020-12-30 20:22:19 +0900

    Update AlertAction 메서드 추가
    메세지 파라미터 없이 forwarding하는 기능 추가

--- src/main/java/com/ticodev/action/AlertAction.java
+++ src/main/java/com/ticodev/action/AlertAction.java
@@ -10,8 +10,21 @@ public class AlertAction {
         return new ActionForward(false, "/alert.jsp");
     }
 
+    public static ActionForward forward(boolean redirect) {
+        return new ActionForward(redirect, "/alert.jsp");
+    }
+
+    public static ActionForward forward() {
+        return new ActionForward(false, "/alert.jsp");
+    }
+
     public static ActionForward forwardBeforePage(HttpServletRequest request, String msg) {
         request.setAttribute("msg", msg);
+        request.setAttribute("url", request.getHeader("Referer"));
+        return new ActionForward(false, "/alert.jsp");
+    }
+
+    public static ActionForward forwardBeforePage(HttpServletRequest request) {
         request.setAttribute("url", request.getHeader("Referer"));
         return new ActionForward(false, "/alert.jsp");
     }
@@ -19,14 +32,6 @@ public class AlertAction {
     public static ActionForward forwardError(HttpServletRequest request) {
         request.setAttribute("msg", "잘못된 경로입니다.");
         request.setAttribute("url", request.getHeader("Referer"));
-        return new ActionForward(false, "/alert.jsp");
-    }
-
-    public static ActionForward forward(boolean redirect) {
-        return new ActionForward(redirect, "/alert.jsp");
-    }
-
-    public static ActionForward forward() {
         return new ActionForward(false, "/alert.jsp");
     }
 
